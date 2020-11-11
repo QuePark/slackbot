f56d29233040f6e35d85bd80891166c6640b6ba1
QuePark <gyuhapark11@gmail.com>
2020-11-11 11:20:09 +0900

    Add information with email and github ID

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
-import Slack from 'slack-node';
-import schdulePkg from 'node-schedule';
-import RtmPkg from '@slack/rtm-api';
+import Slack from "slack-node";
+import schdulePkg from "node-schedule";
+import RtmPkg from "@slack/rtm-api";
 
 // personal function, command, contents, information
-import { names, getRoles } from './yejiseo/lotsRole.js';
-import { command } from './yejiseo/command.js';
-import { contents } from './yejiseo/contents.js';
-import { information } from './yejiseo/information.js';
+import { names, getRoles } from "./yejiseo/lotsRole.js";
+import { command } from "./yejiseo/command.js";
+import { contents } from "./yejiseo/contents.js";
+import { information } from "./yejiseo/information.js";
 
 // get myToken, slackBot imgage, and channels to send a messages
 const {
@@ -44,6 +44,7 @@ const {
 	test,
 	server,
 	client,
+	fullInfo,
 } = command;
 
 // get a messages to send by command
@@ -62,6 +63,7 @@ const {
 	runTest,
 	serverOfGithub,
 	clientOfGithub,
+	callInfo,
 } = contents;
 
 // get a function and class
@@ -76,7 +78,7 @@ const slack = new Slack(token);
 // function to send message
 const send = async (message, channel = selfChannel) => {
 	slack.api(
-		'chat.postMessage',
+		"chat.postMessage",
 		{
 			username: botName, // 슬랙에 표시될 봇이름
 			text: message[Math.floor(Math.random() * message.length)],
@@ -98,32 +100,32 @@ send([`${time}`], selfChannel);
  * Just one message is allowed in a second.
  */
 // Schedule Jobs
-scheduleJob('00 50 08 * * 1-5', () => {
+scheduleJob("00 50 08 * * 1-5", () => {
 	send(greetingText, teamChannel);
 });
-scheduleJob('02 50 08 * * 1-5', () => {
-	send(['시프티 출근했습니까?'], teamChannel);
+scheduleJob("02 50 08 * * 1-5", () => {
+	send(["시프티 출근했습니까?"], teamChannel);
 });
-scheduleJob('00 09 * * 1-5', () => {
-	send(['시프티 출근하셨냐구요'], teamChannel);
+scheduleJob("00 09 * * 1-5", () => {
+	send(["시프티 출근하셨냐구요"], teamChannel);
 });
-scheduleJob('55 11 * * 1-5', () => {
+scheduleJob("55 11 * * 1-5", () => {
 	send(lunchText, generalChannel);
 });
-scheduleJob('00 12 * * 1-5', () => {
+scheduleJob("00 12 * * 1-5", () => {
 	send(lunchText, teamChannel);
 });
-scheduleJob('05 18 * * 1-5', () => {
+scheduleJob("05 18 * * 1-5", () => {
 	send(dinnerText, teamChannel);
 });
-scheduleJob('10 18 * * 1-5', () => {
+scheduleJob("10 18 * * 1-5", () => {
 	send(dinnerText, generalChannel);
 });
-scheduleJob('00 18 * * 1-5', () => {
-	send(['시프티 하셨습니까?'], teamChannel);
+scheduleJob("00 18 * * 1-5", () => {
+	send(["시프티 하셨습니까?"], teamChannel);
 });
-scheduleJob('00 20 * * 1-5', () => {
-	send(['시프티 퇴근하셨냐구요'], teamChannel);
+scheduleJob("00 20 * * 1-5", () => {
+	send(["시프티 퇴근하셨냐구요"], teamChannel);
 });
 
 // send a message that I want to send other channel that I want by botName
@@ -153,17 +155,17 @@ const sendText = (text) => {
 	let searchFlag = false;
 	text.reduce((a, c) => {
 		if (search.includes(c)) {
-			send(['내가 좀 찾아봤어.'], channel);
+			send(["내가 좀 찾아봤어."], channel);
 			searchFlag = true;
 		}
-		if (c !== '<@U019ZCZGQ1F>' && !limitText.includes(c)) {
+		if (c !== "<@U019ZCZGQ1F>" && !limitText.includes(c)) {
 			newText.push(c);
 		}
-	}, '');
+	}, "");
 	if (searchFlag) {
 		setTimeout(() => sendSearchResult(newText, channel), 1000);
 	} else {
-		setTimeout(() => send([newText.join(' ')], channel), 1000);
+		setTimeout(() => send([newText.join(" ")], channel), 1000);
 	}
 };
 
@@ -173,33 +175,33 @@ const sendSearchResult = (textArr, channel) => {
 		(x) => !search.includes(x) && !sendMessage.includes(x),
 	);
 	let searchEngine;
-	if (tmpArr[0] === '네이버') {
+	if (tmpArr[0] === "네이버") {
 		searchEngine = tmpArr.shift();
 		const naver = [
 			`https://search.naver.com/search.naver?ie=UTF-8&query=${tmpArr.join(
-				'+',
+				"+",
 			)}&sm=chr_hty`,
 		];
 		send(naver, channel);
-	} else if (tmpArr[0] === '다음') {
+	} else if (tmpArr[0] === "다음") {
 		searchEngine = tmpArr.shift();
 		const daum = [
-			`https://search.daum.net/search?w=tot&&q=${tmpArr.join('+')}`,
+			`https://search.daum.net/search?w=tot&&q=${tmpArr.join("+")}`,
 		];
 		send(daum, channel);
-	} else if (tmpArr[0] === '유튜브' || tmpArr[0] === '유튭') {
+	} else if (tmpArr[0] === "유튜브" || tmpArr[0] === "유튭") {
 		searchEngine = tmpArr.shift();
 		const youtube = [
-			`https://www.youtube.com/results?search_query=${tmpArr.join('+')}`,
+			`https://www.youtube.com/results?search_query=${tmpArr.join("+")}`,
 		];
 		send(youtube, channel);
-	} else if (tmpArr[0] === '구글') {
+	} else if (tmpArr[0] === "구글") {
 		searchEngine = tmpArr.shift();
-		const google = [`https://www.google.com/search?q=${tmpArr.join('+')}`];
+		const google = [`https://www.google.com/search?q=${tmpArr.join("+")}`];
 		send(google, channel);
 	} else {
-		searchEngine = '구글';
-		const google = [`https://www.google.com/search?q=${tmpArr.join('+')}`];
+		searchEngine = "구글";
+		const google = [`https://www.google.com/search?q=${tmpArr.join("+")}`];
 		send(google, channel);
 	}
 };
@@ -208,8 +210,8 @@ const sendSearchResult = (textArr, channel) => {
 const rtm = new RTMClient(token);
 rtm.start();
 
-rtm.on('message', (message) => {
-	console.log('---------------------------message: ', message);
+rtm.on("message", (message) => {
+	console.log("---------------------------message: ", message);
 	if (!message.hidden) {
 		let text = message.text;
 		if (
@@ -217,23 +219,23 @@ rtm.on('message', (message) => {
 			message.channel === teamChannel ||
 			message.channel === selfChannel
 		) {
-			if (text.split(' ').some((x) => search.includes(x))) {
-				sendSearchResult(text.split(' '), message.channel);
+			if (text.split(" ").some((x) => search.includes(x))) {
+				sendSearchResult(text.split(" "), message.channel);
 			}
-			if (text.split(' ').some((x) => x === '노션!')) {
+			if (text.split(" ").some((x) => x === "노션!")) {
 				send([notionUrl], message.channel);
 			}
-			if (text.split(' ').includes('<@U019ZCZGQ1F>')) {
-				send(['바쁘니까 호출하지마시죠'], message.channel);
+			if (text.split(" ").includes("<@U019ZCZGQ1F>")) {
+				send(["바쁘니까 호출하지마시죠"], message.channel);
 			}
 		}
 		if (
 			message.channel === generalChannel &&
-			text.split(' ').some((x) => name.includes(x))
+			text.split(" ").some((x) => name.includes(x))
 		) {
-			text = text.split(' ');
+			text = text.split(" ");
 			if (text.some((x) => hi.includes(x))) {
-				send(['부르지 말아주세요. 아직 남이니까.'], generalChannel);
+				send(["부르지 말아주세요. 아직 남이니까."], generalChannel);
 			} else if (text.some((x) => booksRecommend.includes(x))) {
 				send(bookList, generalChannel);
 			} else if (text.some((x) => mogacko.includes(x))) {
@@ -247,8 +249,8 @@ rtm.on('message', (message) => {
 			} else {
 				tmpChannel = teamChannel;
 			}
-			if (text.split(' ').filter((x) => name.includes(x)).length > 0) {
-				text = text.split(' ');
+			if (text.split(" ").filter((x) => name.includes(x)).length > 0) {
+				text = text.split(" ");
 				text.shift();
 				if (text.some((x) => github.includes(x))) {
 					send(linkOfGithub, tmpChannel);
@@ -286,7 +288,7 @@ rtm.on('message', (message) => {
 				} else if (text.some((x) => story.includes(x))) {
 					send(storyList, tmpChannel);
 				} else if (text.some((x) => call.includes(x))) {
-					send([teamMember.join(' ')], tmpChannel);
+					send([teamMember.join(" ")], tmpChannel);
 				} else if (
 					tmpChannel === selfChannel &&
 					text.some((x) => sendMessage.includes(x))
@@ -296,9 +298,11 @@ rtm.on('message', (message) => {
 					send(serverOfGithub, tmpChannel);
 				} else if (text.some((x) => client.includes(x))) {
 					send(clientOfGithub, tmpChannel);
+				} else if (text.some((x) => fullInfo.includes(x))) {
+					send(callInfo, tmpChannel);
 				} else if (text.some((x) => test.includes(x))) {
 					send(runTest, tmpChannel);
-				} else if (text.some((x) => x === '현재시각')) {
+				} else if (text.some((x) => x === "현재시각")) {
 					time = new Date();
 					send([String(time)], tmpChannel);
 				}
